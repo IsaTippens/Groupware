b38a3dac9e3c3edbb6363444178ace6f169976ff
IsaTippens <isatippens2@gmail.com>
2022-08-16 20:12:27 +0200

    Example code

---
+++ core/template/example.py
@@ -0,0 +1,5 @@
+# Example showing how to right code for testing
+# Look at tests/template/test_example.py
+
+def add_numbers(a: int, b: int):
+    return a + b
---
+++ tests/template/test_example.py
@@ -0,0 +1,11 @@
+from core.template.example import add_numbers
+
+# Example for how tests should be setup
+# In the terminal run 'pytest'
+# This will succeed
+def test_add():
+    assert add_numbers(400, 20) == 420
+
+# This will fail
+def test_add_2():
+    assert add_numbers(400, 20) == 1337  
