5945bd2e2cef6b747884fc86c92f543a404ddf1c
Isa Tippens <isatippens2@gmail.com>
2022-08-18 23:37:54 +0000

    Add Booking model

---
+++ core/models/booking.py
@@ -0,0 +1,12 @@
+from core.models.movie import Movie
+
+class Booking:
+    def __init__(self):
+        self.seats = []
+        self.movie = None
+        self.time = ""
+    
+    def create(self, movie: Movie, seats: list[str], time: str):
+        self.seats = seats
+        self.movie = movie
+        self.time = time
