592af918b4e6bca52c010dc55762b5efbd0c951a
IsaTippens <isatippens2@gmail.com>
2022-08-24 15:38:39 +0200

    Remove print message

--- app/screen/screen_manager.py
+++ app/screen/screen_manager.py
@@ -9,7 +9,6 @@ class ScreenManager():
         pass
 
     def start(self):
-        super().start()
         while len(self.screens) > 0:
             current = self.screens[-1]
             current.next = None
@@ -19,7 +18,6 @@ class ScreenManager():
                 current.previous_screen.next = None
 
             if current.next is not None:
-                print("Adding ", current.next)
                 self.addScreen(current.next)
                 continue
             self.screens.pop()
