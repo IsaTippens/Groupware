9f6284b7f772490de47c0e6339d0fe9d9348d48b
IsaTippens <isatippens2@gmail.com>
2022-08-26 22:29:11 +0200

    Added MovieService singleton

---
+++ app/globals.py
@@ -0,0 +1,12 @@
+from core.repositories import MovieRepository as MR
+from core.services import MovieService as MS
+
+"""
+    Global Singletons to be used by the screens
+
+    'X'Service should be the class that you use to interact with the data
+    see app/movies.py for an example of how to use this class
+
+    There will be more services added to handle bookings, seats available, anything to make life hopefully easier :)
+"""
+MovieService = MS(MR("data/movies.json"))
