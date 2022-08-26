5aef947c62bdaf40b3b2b78990fd7163171195ca
IsaTippens <isatippens2@gmail.com>
2022-08-26 20:39:15 +0200

    Add initial value for movie type

--- core/models/movie.py
+++ core/models/movie.py
@@ -1,6 +1,6 @@
 class Movie:
 
-    def __init__(self, id: int, name: str, movieType : int):
+    def __init__(self, id: int, name: str, movieType: int = 1):
         self.id = id
         self.name = name
         self.type = movieType
