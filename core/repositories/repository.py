231f2f6f81bf83de3c4b15cee895028756f7a61f
IsaTippens <isatippens2@gmail.com>
2022-08-26 16:31:50 +0200

    Add service and repository classes

---
+++ core/repositories/repository.py
@@ -0,0 +1,18 @@
+class Repository:
+    def __init__(self):
+        pass
+
+    def get_all(self):
+        pass
+
+    def get(self,name: str):
+        pass
+
+    def add(self,value):
+        pass
+
+    def update(self,value):
+        pass
+
+    def delete(self,value):
+        pass
---
+++ core/services/service.py
@@ -0,0 +1,18 @@
+class Service():
+    def __init__(self):
+        pass
+    
+    def getAll(self):
+        pass
+
+    def get(self,name: str):
+        pass
+
+    def add(self,value):
+        pass
+
+    def update(self,value):
+        pass
+
+    def delete(self,value):
+        pass
