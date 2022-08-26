7b23492f8b03f2d4ff1b24d082051b255d540f88
IsaTippens <isatippens2@gmail.com>
2022-08-26 21:13:16 +0200

    Add description to movie viewmodel

--- core/viewmodels/movie.py
+++ core/viewmodels/movie.py
@@ -1,8 +1,9 @@
 from core.enums import MovieType
 class Movie:
 
-    def __init__(self, id: int, name: str, movieType : MovieType = MovieType.BASIC2D):
+    def __init__(self, id: int, name: str, description: str, movieType : MovieType = MovieType.BASIC2D):
         self.id = id
         self.name = name
+        self.description = description
         self.type = movieType
         pass
