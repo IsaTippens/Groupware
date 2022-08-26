a95030682290b66171ca7d9e17a27743c83663eb
IsaTippens <isatippens2@gmail.com>
2022-08-26 17:26:06 +0200

    Add save_json method

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,2 +1,2 @@
-from core.utils.util import load_json
-__all__ = ['load_json']
+from core.utils.util import *
+__all__ = ['load_json', 'save_json']
--- core/utils/util.py
+++ core/utils/util.py
@@ -4,3 +4,8 @@ import json
 
 def load_json(fileObj: TextIOWrapper):
     return json.load(fileObj)
+
+def save_json(fileName: str, data: dict):
+    with open(fileName, 'w') as f:
+        json.dump(data, f)
+    
