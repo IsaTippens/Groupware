ebac0d0164d98d0f7b2defc3e8ff7f0490f6be22
IsaTippens <isatippens2@gmail.com>
2022-08-26 22:28:51 +0200

    Added service documentation

--- core/services/service.py
+++ core/services/service.py
@@ -1,4 +1,12 @@
 class Service():
+    """
+        Services abstract the interaction being data storage/retrieval from a frontend
+        Data is parsed to the relevant views as well as additional processing
+        It does not care where or how the data is stored and retrieved, thats the job of the repositories
+        As long as the repository provides the expected data type, services can do their job
+
+        Frontends could be a gui/rest api/ or a terminal interface like the one we're doing.
+    """
     def __init__(self):
         pass
     
