ea4c075d6ae8526c1ca1d59faad3ab7e62701d0e
IsaTippens <isatippens2@gmail.com>
2022-08-31 13:28:34 +0200

    Add state dict

--- app/globals.py
+++ app/globals.py
@@ -1,3 +1,4 @@
+from typing import Any
 from core.repositories import MovieRepository as MR
 from core.services import MovieService as MS
 
@@ -10,3 +11,5 @@ from core.services import MovieService as MS
     There will be more services added to handle bookings, seats available, anything to make life hopefully easier :)
 """
 MovieService = MS(MR("data/movies.json"))
+
+State = dict[str, Any]()
