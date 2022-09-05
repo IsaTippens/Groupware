b77a400577bb39bcda04c19cd4190dbdfab075bb
IsaTippens <isatippens2@gmail.com>
2022-09-06 00:28:30 +0200

    Add Booking exports

--- core/repositories/__init__.py
+++ core/repositories/__init__.py
@@ -1,3 +1,4 @@
 from core.repositories.movie_repository import MovieRepository
+from core.repositories.booking_repository import BookingRepository
 
-__all__ = ['MovieRepository']
+__all__ = ['MovieRepository', 'BookingRepository']
--- core/services/__init__.py
+++ core/services/__init__.py
@@ -1,3 +1,4 @@
 from core.services.movie_service import MovieService
+from core.services.booking_service import BookingService
 
-__all__ = ['MovieService']
+__all__ = ['MovieService', 'BookingService']
