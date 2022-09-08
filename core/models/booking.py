37c319351e430f59119c1394093952b6a3229336
IsaTippens <isatippens2@gmail.com>
2022-09-08 18:16:12 +0200

    Add typing to ticket models

--- core/models/booking.py
+++ core/models/booking.py
@@ -3,8 +3,8 @@ from core.models import Movie
 class Booking:
     def __init__(self):
         self.id = 0
-        self.seats = []
-        self.movie = None
-        self.time = ""
-        self.owner = ""
-        self.date = ""
+        self.seats: list[str] = []
+        self.movie: Movie = None
+        self.time: str = ""
+        self.owner: str = ""
+        self.date: str = ""
--- core/viewmodels/ticket.py
+++ core/viewmodels/ticket.py
@@ -1,11 +1,15 @@
+
+from core.viewmodels import Movie
+
+
 class Ticket:
     def __init__(self):
-        self.id = 0
-        self.seats = []
-        self.movie = None
-        self.time = ""
-        self.date = ""
-        self.owner = ""
+        self.id:int = 0
+        self.seats: list[str] = []
+        self.movie: Movie = None
+        self.time: str = ""
+        self.date: str = ""
+        self.owner: str = ""
     
     def __str__(self):
         return f"Ticket {self.id} for {self.movie} at {self.time} for {self.owner} with {self.seats}"
