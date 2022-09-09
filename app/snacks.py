97f155e2480340188521fc0fb4331a74b32b0ab8
ColemanAlexander <4047081@uwc.ac.za>
2022-09-09 14:55:09 +0200

    with snacks

---
+++ app/snacks.py
@@ -0,0 +1,34 @@
+from app.payment import Payment
+from app.screen.titled_screen import TitledScreen
+from app.Test import Test
+from app.movies import MovieScreen
+
+from app.globals import State
+
+class Snacks(TitledScreen):
+    def __init__(self):
+        super().__init__("Snacks")
+    def start(self):
+        super().start()
+        print("The state is: ", State.get("test", "Empty"))
+        print()
+        snacks = []
+        while(True):
+            print("Select snack:")
+            print("1\tPopcorn")
+            print("2\tCandy")
+            print("3\tSlushie")
+            print("4\tCancel")
+            print()
+            num = input("Enter a number: ")
+            if num == "1":
+                snacks.append("Popcorn")
+            if num == "2":
+                snacks.append("Candy")
+            if num == "3":
+                snacks.append("Slushie")
+            if num == "4":
+                State["TICKET"].snacks = snacks
+                break
+                
+        return self.navigate(Payment()) 
