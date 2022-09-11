a648b29e348f74a86ab177c53ab686115aa9f69b
IsaTippens <isatippens2@gmail.com>
2022-09-11 17:43:26 +0200

    Add str method to movie type

--- core/enums/movie_type.py
+++ core/enums/movie_type.py
@@ -5,3 +5,14 @@ class MovieType(Enum):
     BASIC3D = 2
     IMAX2D = 3
     IMAX3D = 4
+
+    def __str__(self):
+        if (self.value == 1):
+            return "2D"
+        elif (self.value == 2):
+            return "3D"
+        elif (self.value == 3):
+            return "IMAX 2D"
+        elif (self.value == 4):
+            return "IMAX 3D"
+        return "INVALID"
